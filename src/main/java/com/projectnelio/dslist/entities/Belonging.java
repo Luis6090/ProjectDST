f282a1af22649a6483c2324e79ea2a9aa6176707
Luis Felipe <stefanluis78@gmail.com>
2024-11-15 15:37:01 -0300

    Domain model

---
+++ src/main/java/com/projectnelio/dslist/entities/Belonging.java
@@ -0,0 +1,52 @@
+package com.projectnelio.dslist.entities;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_belonging")
+public class Belonging {
+    private Integer position;
+
+    @EmbeddedId
+    private BelongingPK id = new BelongingPK();
+
+    public Belonging(){}
+
+    public Belonging(Integer position,Game game,GameList list) {
+        this.position = position;
+        id.setGame(game);
+        id.setList(list);
+    }
+
+    public Integer getPosition() {
+        return position;
+    }
+
+    public void setPosition(Integer position) {
+        this.position = position;
+    }
+
+    public BelongingPK getId() {
+        return id;
+    }
+
+    public void setId(BelongingPK id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Belonging belonging)) return false;
+        return Objects.equals(id, belonging.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
